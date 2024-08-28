0993d5117ce8db247b0c7b0fe7c9db35ab2b956c
Darshan Rathi <94820479+darshanrathibhl@users.noreply.github.com>
2024-08-28 10:23:16 +0530

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as a newer version.
+
+// alert(3+3)   //hum use kar rahe h nodejs and ye only browser me hi chlta h.
+
+/*console.log(
+    3+
+    3);
+*/      // code readability bilkul bhi nhi h but ye code to chalega.
+
+let name="darshan"  //string type
+let age=16          //integer type
+let isLoggedIn=false        // boolean type
+
+
+// number => 2 to power 53
+// bigint
+// string=""
+// boolean=true/false
+// null=standalone value
+// undefined =>
+// symbol =>unique
+
+console.log(typeof"darshan") ;
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
