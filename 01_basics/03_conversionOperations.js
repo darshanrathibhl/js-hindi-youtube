718d8ad27c53ff298f5be91e14dc153bea76bbbc
Darshan Rathi <94820479+darshanrathibhl@users.noreply.github.com>
2024-08-28 14:15:50 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score="33abc"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+
+console.log(typeof valueInNumber);
+
+// conversion from particular to number
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1    //false => 0   
+// null => 0
+//undefined => NaN
+    
+    
+    //let x=Number(null)
+    //console.log(x);                   // 0
+
+    // let y=Number(undefined)
+    // console.log(y);
+    
+// let isLoggedIn=1;
+// console.log(Boolean(isLoggedIn));                         // true
+
+// let abc=""                                                    // false
+// console.log(Boolean(abc));
+
+
+        // 1 -> true   //0->false
+        //"" -> false
+        //"xyz" -> true
