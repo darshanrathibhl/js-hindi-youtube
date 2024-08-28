63145f24c0a095fc2968980aa8376b0549a14357
Darshan Rathi <94820479+darshanrathibhl@users.noreply.github.com>
2024-08-28 10:23:45 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let xyz;
+console.log(xyz)//undefined because in js variable have undefind value at prior.
+
+
+
+
+// accountId = 2 // not allowed 
+accountEmail = "abc@gmail.com"
+accountPassword = "54321";
+accountCity = "Pune"
+console.table([accountEmail, accountId, accountPassword, accountCity]);
+// console.log(accountPassword);
+// console.log(accountEmail)
+// console.log(accountId)
+// console.log(accountCity);
